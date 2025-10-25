c2a16a0957aef469ea9ce64ceebfcb7bca845db6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-10-25 14:20:45 +0530

    documentation is nice

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/jung-kurt/gofpdf"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeA4, "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, "Hello, world!")
+	err := pdf.OutputFileAndClose("hello.pdf")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("PDF generated successfully")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MdSadiqMd/DynamicPDF_Generator_Go
 
 go 1.24.9
+
+require github.com/jung-kurt/gofpdf v1.16.2 // indirect
